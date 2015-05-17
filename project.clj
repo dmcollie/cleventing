8101cc0813232f955b497c190521c982d8a2b608
David Collie <dmcollie@me.com>
2015-05-17 23:11:01 +0100

    Updated project properties ready for publishing to clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cleventing "0.1.0-SNAPSHOT"
-  :description "Event Sourcing"
-  :url "http://timothypratley.blogspot.com"
+(defproject dmcollie/cleventing "0.1.0"
+  :description "Event Sourcing library forked from https://github.com/timothypratley/cleventing"
+  :url "http://www.davidcollie.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
